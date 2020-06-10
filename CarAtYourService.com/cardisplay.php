ce2967f3a915078739aa00a965e1ac7a45fa3238
isaac1249 <52692233+isaac1249@users.noreply.github.com>
2020-06-10 22:33:06 +0800

    Add files via upload

---
+++ CarAtYourService.com/cardisplay.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel=stylesheet type="text/css" href="style.css">
+<title>首頁</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+</head>
+<body>
+<div class="navbar">
+<a href="carrental.php">首頁</a>
+<a href="contact.php">聯絡我們</a>
+<a href="login.php" class="right">登入</a>
+<a href="new.php" class="right">註冊</a>
+</div>
+<div class="bg-image2"></div>
+
+
+<div class="bg-text">
+
+  <ul class="thumbnails">
+      <li>
+          <div class="thumbnail">
+              <img src="../TOYOTA AURIS.jpg" alt="*">
+              <div class="caption">
+                  <h3>TOYOTA AURIS</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+          <div class="thumbnail">
+              <img src="../Toyota Corolla Altis.jpg" alt="*">
+              <div class="caption">
+                  <h3>TOYOTA COROLLA ALTIS</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+          <div class="thumbnail">
+              <img src="../TOYOTA New ALTIS.jpeg" alt="*">
+              <div class="caption">
+                  <h3>TOYOTA New ALTIS</h3>
+                  <p> Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+          <div class="thumbnail">
+              <img src="../Toyota new Yaris.jpg" alt="*">
+              <div class="caption">
+                  <h3>TOYOTA New YARIS</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+          <div class="thumbnail">
+              <img src="../HONDA FIS S.jpeg" alt="*">
+              <div class="caption">
+                  <h3>HONDA FIS S</h3>
+                  <p>Cras justo odio, dapibus ac facilisis in. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+          <div class="thumbnail">
+              <img src="../MAZDA 3 5D.jpeg" alt="*">
+              <div class="caption">
+                  <h3>MAZDA 3 5D</h3>
+                  <p> Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+          </div>
+      </li>
+      <li>
+            <div class="thumbnail">
+                <img src="../MAZDA 3 4D.jpg" alt="*">
+                <div class="caption">
+                    <h3>MAZDA 3 4D</h3>
+                    <p> Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </div>
+            </div>
+        </li>
+        <li>
+              <div class="thumbnail">
+                  <img src="../Nissan LIVINA.jpg" alt="*">
+                  <div class="caption">
+                      <h3>Nissan LIVINA</h3>
+                      <p> Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  </div>
+              </div>
+          </li>
+  </ul>
+
+
+
+
+
+</div>
+
+
+
+</body>
+</html>
+
+</div>
